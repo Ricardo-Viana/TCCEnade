c580117150879b68f36943db7ef2c13f316651c0
Ricardo Viana <ricardo.macedo@dcx.ufpb.br>
2024-10-08 14:24:06 -0300

    Adicionado link para baixar tabela de conceito Enade

--- testeConsumoLinkHtml.py
+++ testeConsumoLinkHtml.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 def main():
     # URL da página web que contém os links de download
-    url = 'https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/indicadores-educacionais/indicadores-de-qualidade-da-educacao-superior'
+    url = 'https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/indicadores-educacionais/indicadores-de-qualidade-da-educacao-superior/2018' # Link fixo só muda o último componente que depende do ano
 
     # Fazer a solicitação HTTP
     response = requests.get(url)
@@ -18,15 +18,11 @@ def main():
         # Encontrar todos os links de download (normalmente, estão em tags <a>)
         links = soup.find_all('a', href=True)
 
-        div = soup.find('a', class_='external-link')
-
-        print(div)
-
         # Iterar sobre os links encontrados e exibir ou filtrar os links de download
         for link in links:
             href = link['href']
             # Filtrar os links que contenham a palavra "download" ou outro critério desejado
-            if 'download' in href or href.endswith('.zip'):  # Exemplo de filtro
+            if 'download' in href and (href.endswith('.xlsx') or href.endswith('.xls')):  # Exemplo de filtro
                 print(href)
     else:
         print(f"Erro ao acessar a página: {response.status_code}")
