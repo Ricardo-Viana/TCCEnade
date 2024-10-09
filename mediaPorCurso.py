47aa192c7c4e39002f9f9f3c3a968959c204832a
Ricardo Viana <ricardo.viana18@gmail.com>
2024-10-08 21:23:12 -0300

    Finalizado mediaPorCurso

--- mediaPorCurso.py
+++ mediaPorCurso.py
@@ -1,59 +1,113 @@
 import pandas as pd
 import numpy as np
+import requests
+from io import BytesIO
+from zipfile import ZipFile
+import os
+import re
 
-def processa_respostas(ano, cod_grupo, prefixo_coluna, valores_na, caminho_base):
-    df = pd.read_csv(f'{caminho_base}/Microdados{ano}/microdados{ano}_arq1.txt', delimiter=';')
+def processa_respostas(ano, cod_grupo, valores_na, tipo_questao):
+    if ano == 2021:
+        url = 'https://download.inep.gov.br/microdados/microdados_enade_2021.zip'
+    else:
+        url = f'https://download.inep.gov.br/microdados/microdados_enade_{ano}_LGPD.zip'
+
+    dir_path = f'./enade{ano}'
+    if not os.listdir(dir_path):
+        os.makedirs(dir_path)
+    
+        filebytes = BytesIO(
+            requests.get(url).content
+        )
+
+        myzip = ZipFile(filebytes)
+        myzip.extractall(dir_path)    
+
+    if ano == 2021:
+        df = pd.read_csv('enade2021/microdados_Enade_2021/2. DADOS/microdados2021_arq1.txt', delimiter = ';')
+    else:
+        df = pd.read_csv(f'enade{ano}/microdados_Enade_{ano}_LGPD/2. DADOS/microdados{ano}_arq1.txt', delimiter = ';')
     filtered_df = df[df['CO_GRUPO'] == cod_grupo]
     unique_courses = filtered_df['CO_CURSO'].unique()
-    df_respostas = pd.read_csv(f'{caminho_base}/Microdados{ano}/microdados{ano}_arq3.txt', delimiter=';')
 
-    # Filtrar cursos únicos
-    df_respostas_filtered = df_respostas[df_respostas['CO_CURSO'].isin(unique_courses)]
+    if tipo_questao == 'pandemia':
+        
+        if ano == 2021:
+            files = os.listdir('enade2021/microdados_Enade_2021/2. DADOS')
+            files_sorted_by_number = sorted(files, key=lambda x: int(re.search(fr'microdados2021_arq(\d+)', x).group(1)))
+            latest_file = files_sorted_by_number[-1]
+            df_respostas = pd.read_csv(f'enade2021/microdados_Enade_2021/2. DADOS/{latest_file}', delimiter = ';')
+        elif ano > 2021:
+            files = os.listdir(f'enade{ano}/microdados_Enade_{ano}/2. DADOS')
+            files_sorted_by_number = sorted(files, key=lambda x: int(re.search(fr'microdados{ano}_arq(\d+)', x).group(1)))
+            latest_file = files_sorted_by_number[-1]    
+            df_respostas = pd.read_csv(f'enade{ano}/microdados_Enade_{ano}_LGPD/2. DADOS/{latest_file}', delimiter = ';')
+        else:
+            raise Exception('Ano não está incluso na pandemia')
 
-    # Identificar as colunas de respostas com base no prefixo fornecido
-    colunas_respostas = [col for col in df_respostas_filtered.columns if col.startswith(prefixo_coluna)]
+        df_respostas_filtered = df_respostas[df_respostas['CO_CURSO'].isin(unique_courses)]
 
-    # Substituir valores por escala de 1 a 5
-    df_respostas_filtered[colunas_respostas] = df_respostas_filtered[colunas_respostas].replace({'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5})
+        colunas_respostas = [col for col in df_respostas_filtered.columns if col.startswith('QE_')]
 
-    # Substituir valores não numéricos por NaN
+        novos_nomes_colunas = [f"QE_{i+1}" for i in range(len(colunas_respostas))]
+
+        renomear_dict = dict(zip(colunas_respostas, novos_nomes_colunas))
+
+        df_respostas_filtered.rename(columns=renomear_dict, inplace=True)
+
+        colunas_respostas = novos_nomes_colunas
+
+    elif tipo_questao == 'percepção prova':
+
+        if ano == 2021:
+            df_respostas = pd.read_csv(f'enade2021/microdados_Enade_2021/2. DADOS/microdados2021_arq3.txt', delimiter = ';')
+        else:    
+            df_respostas = pd.read_csv(f'enade{ano}/microdados_Enade_{ano}_LGPD/2. DADOS/microdados{ano}_arq3.txt', delimiter = ';')
+        
+        df_respostas_filtered = df_respostas[df_respostas['CO_CURSO'].isin(unique_courses)]
+
+        colunas_respostas = [col for col in df_respostas_filtered.columns if col.startswith('CO_RS')]
+
+        df_respostas_filtered[colunas_respostas] = df_respostas_filtered[colunas_respostas].replace({'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5})
+
+
+    elif tipo_questao == 'processo formativo':
+        if ano == 2021:
+            df_respostas = pd.read_csv(f'enade2021/microdados_Enade_2021/2. DADOS/microdados2021_arq4.txt', delimiter = ';')
+        else:
+            df_respostas = pd.read_csv(f'enade{ano}/microdados_Enade_{ano}_LGPD/2. DADOS/microdados{ano}_arq4.txt', delimiter = ';')
+
+        df_respostas_filtered = df_respostas[df_respostas['CO_CURSO'].isin(unique_courses)]
+
+        colunas_respostas = [col for col in df_respostas_filtered.columns if col.startswith('QE_')]
+        
     df_respostas_filtered[colunas_respostas] = df_respostas_filtered[colunas_respostas].replace(valores_na, np.nan)
 
-    # Remover linhas onde todas as respostas são NaN
     df_respostas_filtered = df_respostas_filtered.dropna(how='all', subset=colunas_respostas)
 
-    # Calcular a média das respostas por curso
     media_respostas = df_respostas_filtered.groupby('CO_CURSO')[colunas_respostas].mean().reset_index()
 
-    # Adicionar a coluna 'CO_GRUPO' ao resultado
     media_respostas['CO_GRUPO'] = cod_grupo
 
     return media_respostas
 
-# Solicitar entrada do usuário para os anos a serem analisados
 anos_input = input("Digite os anos a serem analisados (separados por vírgula, ex: 2019,2021,2022): ")
 anos_list = [int(ano.strip()) for ano in anos_input.split(',')]
 
-# Solicitar entrada do usuário para os valores a serem substituídos por np.nan
 valores_na_input = input("Digite os valores a serem substituídos por NaN (separados por vírgula, ex: '*', '.'): ")
 valores_na = [valor.strip().strip("'") for valor in valores_na_input.split(',')]
 
-# Carregar o arquivo base com os códigos de grupo
-df_cinerotulo = pd.read_csv('/content/drive/MyDrive/TCCENADEARQUIVOS/cinerotuloenade.csv')
+tipo_questao = input("Digite qual tipo de questão você quer gerar um relatório (pandemia, percepção prova ou processo formativo): ").lower()
 
-# Criar DataFrame para consolidar as métricas
+df_cinerotulo = pd.read_csv('cinerotuloenade.csv')
+
 metricas = pd.DataFrame()
 
-# Caminho base para os arquivos de microdados
-caminho_base = '/content/drive/MyDrive/TCCENADEARQUIVOS'
-
-# Processar para cada ano e grupo
 for ano in anos_list:
-    if ano in anos_colunas:
-        df_enade = df_cinerotulo[f'cod_enade_{ano}'].dropna()
-        for cod_grupo in df_enade:
-            resultado = processa_respostas(ano, cod_grupo, anos_colunas[ano], valores_na, caminho_base)
-            metricas = pd.concat([metricas, resultado], ignore_index=True)
+    df_enade = df_cinerotulo[f'cod_enade_{ano}'].dropna() # Alterar para uma lista circular futuramente
+    for cod_grupo in df_enade:
+        resultado = processa_respostas(ano, cod_grupo, valores_na, tipo_questao)
+        metricas = pd.concat([metricas, resultado], ignore_index=True)
     else:
         print(f"Ano {ano} não está disponível para análise.")
 
